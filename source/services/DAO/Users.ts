38d932adb3ab250f4161339828317d4484469a1a
Franco Speziali <franco@wemakemachines.com>
2022-08-17 12:40:46 +0200

    Updated return types on Users DAO

--- source/services/DAO/Users.ts
+++ source/services/DAO/Users.ts
@@ -2,15 +2,19 @@ import Users from "../../models/User";
 import { UserType } from "../../types/schemas/User";
 
 export default class UsersDAO {
-  public static async getUserByUsername(userName: string): Promise<UserType> {
+  public static async getUserByUsername(
+    userName: string
+  ): Promise<UserType | null> {
     return Users.findOne({ userName }).lean();
   }
 
-  public static async getUserById(id: string): Promise<UserType> {
+  public static async getUserById(id: string): Promise<UserType | null> {
     return Users.findById(id).lean();
   }
 
-  public static async getUserByIdProtected(id: string): Promise<UserType> {
+  public static async getUserByIdProtected(
+    id: string
+  ): Promise<UserType | null> {
     return Users.findById(id).select("-hash").lean();
   }
 }
